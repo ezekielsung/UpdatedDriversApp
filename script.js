93cbcf2734e77b60b2f631f4b00dfb4a28d65e54
ezekielsung <ezekielsung@csus.edu>
2020-11-24 20:08:40 -0800

    added 2nd content

--- script.js
+++ script.js
@@ -23,9 +23,51 @@ window.onclick = function(event) {
 
 function collapse(){
 var x = document.getElementById("collapseInfo");
-if (x.style.display=="block"){
-  x.style.display="none";
+var y = document.getElementById("afterCollapseInfo");
+x.style.display="none";
+y.style.display="block";
+}
+
+function collapse2(){
+var x = document.getElementById("collapseInfo2");
+var y = document.getElementById("afterCollapseInfo2");
+x.style.display="none";
+y.style.display="block";
+}
+
+function back(){
+	var x = document.getElementById("collapseInfo");
+	var y = document.getElementById("afterCollapseInfo");
+	x.style.display="block";
+	y.style.display="none";
+}
+function back2(){
+	var x = document.getElementById("collapseInfo2");
+	var y = document.getElementById("afterCollapseInfo2");
+	x.style.display="block";
+	y.style.display="none";
 }
 
 
+function textCustomer(){
+	var x = document.getElementById("text");
+	x.innerHTML="Sent";
+	x.style.background="gray";
+}
+function textCustomer2(){
+	var x = document.getElementById("text2");
+	x.innerHTML="Sent";
+	x.style.background="gray";
+}
+
+function close2(){
+	console.log("hello");
+	var y = document.getElementById("afterCollapseInfo2");
+	y.style.display="none";
+}
+
+function close(){
+	console.log("hello");
+	var y = document.getElementById("afterCollapseInfo2");
+	y.style.display="none";
 }
